d8d29fb7e9ff4f4dc551e33c0335f8061a571e30
barii <thebarii@gmail.com>
2018-12-08 18:14:45 +0000

    add setupTests.js file to cinfigure Enzyme

--- src/Congrats.test.js
+++ src/Congrats.test.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import Enzyme from 'enzyme';
-import EnzymeAdapter from 'enzyme-adapter-react-16'
 import { findByTestAttr, checkProps } from '../test/testUtils'
 import Congrats from './Congrats'
-
-Enzyme.configure({adapter: new EnzymeAdapter() });
 
 const defaultProps = { success: false };
 
@@ -17,7 +14,7 @@ const defaultProps = { success: false };
  */
 const setup = (props={}) => {
   const setupProps = { ... defaultProps, ...props }
-  return Enzyme.shallow(<Congrats {...props} />)
+  return Enzyme.shallow(<Congrats {...setupProps} />)
 }
 
 it('should render without error', () => {
