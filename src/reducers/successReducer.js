d2d1a1b34f91ea73fe3d541b2d142c922927109e
barii <thebarii@gmail.com>
2018-12-08 21:33:13 +0000

    typo

---
+++ src/reducers/successReducer.js
@@ -0,0 +1,10 @@
+/** 
+ * @function
+ * @param {array} state - Array of guessed words
+ * @param {object} action - action to be reduced
+ * @returns {boolean} - new success state 
+ */
+
+export default (state, action) =>  {
+  return null;
+}
