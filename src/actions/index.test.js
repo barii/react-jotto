5663c7292c12bdb7559042e62af53f05e137ef75
barii <thebarii@gmail.com>
2018-12-09 22:28:23 +0000

    add secretWord action creator and reducer

--- src/actions/index.test.js
+++ src/actions/index.test.js
@@ -1,4 +1,8 @@
-import { correctGuess, actionTypes } from './'
+import moxios from 'moxios'
+
+import { storeFactory } from '../../test/testUtils'
+import { getSecretWord } from './'
+//import { correctGuess, actionTypes } from './'
 
 // describe('Correct guess ', () => {
 //   it('should return an action with the type `CORRECT_GUESS`', () => {
@@ -7,6 +11,31 @@ import { correctGuess, actionTypes } from './'
 //   });
 // });
 
-it('', () => {
-  
+describe('getSecretWord action creator', () => {
+  beforeEach(() => {
+    moxios.install();
+  });
+
+  afterEach(() => {
+    moxios.uninstall();
+  });
+
+  it('should add response word to the state', () => {
+    const secretWord = 'party';
+    const store = storeFactory();
+
+    moxios.wait(() => {
+      const request = moxios.requests.mostRecent();
+      request.respondWith({
+        status: 200,
+        response: secretWord
+      })
+    })
+
+    return store.dispatch(getSecretWord())
+      .then(() => {
+        const newState = store.getState();
+        expect(newState.secretWord).toBe(secretWord);
+      });
+  });
 });
